d6387d130ffa190544a3ea884c2b31eb0e95678a
isissi <isissi_525@hotmail.com>
2022-03-23 13:39:49 -0600

    add function selectpanel

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -28,21 +28,39 @@ const data = [
 ];
 
 class Dashboard extends Component {
-  state = { loading: false };
+  state = {
+    loading: false,
+    focused: null
+  };
+
+  selectPanel(id) {
+    this.setState({
+     focused: id
+    });
+   }
 
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {'dashboard--focused': this.state.focused});  
 
     if (this.state.loading) {
       return <Loading />;
-    } ;
-
-    const panelList = data.map(panel => {
-      return <Panel key={panel.id} id={panel.id} lable={panel.lable} value={panel.value} />
-    })
-
-    return <main className={dashboardClasses}>{panelList}</main>
     }
+
+    const panels = data
+      .filter(
+        panel => this.state.focused === null || this.state.focused === panel.id
+      )
+      .map(panel => (
+        <Panel
+        key={panel.id}
+        label={panel.label}
+        value={panel.value}
+        onSelect={e => this.selectPanel(panel.id)}
+        />
+      ));
+
+    return <main className={dashboardClasses}>{panels}</main>;
+  }
 }
 
 export default Dashboard;
