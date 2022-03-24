baed02fd5b93d96e6817f5b70c39729bec5678b0
isissi <isissi_525@hotmail.com>
2022-03-23 19:07:11 -0600

    update data & import helpers

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,27 +4,33 @@ import classnames from "classnames";
 import Loading from "./Loading";
 import Panel from "./Panel";
 import axios from 'axios';
+import {
+  getTotalInterviews,
+  getLeastPopularTimeSlot,
+  getMostPopularDay,
+  getInterviewsPerDay
+ } from "helpers/selectors";
 
 const data = [
   {
     id: 1,
     label: "Total Interviews",
-    value: 6
+    getValue: getTotalInterviews
   },
   {
     id: 2,
     label: "Least Popular Time Slot",
-    value: "1pm"
+    getValue: getLeastPopularTimeSlot
   },
   {
     id: 3,
     label: "Most Popular Day",
-    value: "Wednesday"
+    getValue: getMostPopularDay
   },
   {
     id: 4,
     label: "Interviews Per Day",
-    value: "2.3"
+    getValue: getMostPopularDay
   }
 ];
 
@@ -85,11 +91,11 @@ class Dashboard extends Component {
         <Panel
         key={panel.id}
         label={panel.label}
-        value={panel.value}
+        value={panel.getValue(this.state)}
         onSelect={e => this.selectPanel(panel.id)}
         />
       ));
-
+        
     return <main className={dashboardClasses}>{panels}</main>;
   }
 }
